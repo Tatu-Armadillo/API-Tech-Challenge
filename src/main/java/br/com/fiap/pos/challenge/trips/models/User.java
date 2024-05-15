61597cea236b7fe346db9ef5a151c35efce034f4
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2024-05-14 21:41:48 -0300

    remove spring security

--- src/main/java/br/com/fiap/pos/challenge/trips/models/User.java
+++ src/main/java/br/com/fiap/pos/challenge/trips/models/User.java
@@ -1,19 +1,12 @@
 package br.com.fiap.pos.challenge.trips.models;
 
-import java.util.Collection;
-import java.util.List;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
 @Table(name = "users")
-public class User implements UserDetails {
+public class User  {
 
     @Id
     @Column(name = "id_user")
@@ -26,52 +19,5 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String password;
 
-    public User() { }
-
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public Long getIdUser() {
-        return idUser;
-    }
-
-    public void setIdUser(Long idUser) {
-        this.idUser = idUser;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 
 }
